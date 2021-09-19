6740bff84a80b3cfc41d3528109ca14f56c46713
tabbylie <dyoung8765@gmail.com>
2021-09-18 21:35:45 -0400

    added the file system !

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,49 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, make_response
+import os
+from os import path
+import json
+from os.path import join, dirname, realpath
 
 
-@app.route('/')
-@app.route('/index')
+def getfiles(directory, dirpath):
+    folders = []
+    files = []
+    for filefolder in directory:
+        filefoldertemp = path.join(dirpath, filefolder.strip())
+        if path.isdir(filefoldertemp):
+            folders.append(filefolder)
+            continue
+        files.append(filefolder)
+    return (folders, files)
+
+
+@app.route('/', methods=["GET", "POST"])
+@app.route('/index', methods=["GET", "POST"])
 def index():
-    user = {'username': 'tabbykin'}
-    return render_template('index.html', user=user)
+    # dirpath = path.join(os.environ['USERPROFILE'], 'Downloads')
+    dirpath = '/root/ftpfolder'
+    directory = request.cookies.get('currentFolder')
+    lastFolder = request.cookies.get('prevFolder')
+    if lastFolder == None:
+        lastFolder = dirpath
+    lastFolder = lastFolder.replace('\\', '\\\\')
+    if directory == None:
+        directory = dirpath
+    directory = directory.replace('\\', '\\\\')
+    direct = os.listdir(directory)
+    folders, files = getfiles(direct, directory)
+
+    return render_template('index.html', folders=folders, files=files, lastfolder=lastFolder, currentFolder=directory)
+
+
+@app.route('/set_cookie', methods=["GET", "POST"])
+def set_cookie():
+    if request.method == "POST":
+        dic = request.get_json()
+        direct = dic['filepath']
+        resp = make_response(render_template('index.html'))
+        resp.set_cookie('prevFolder', dic['currentFolder'])
+        resp.set_cookie('currentFolder', direct)
+        return resp
+    return redirect('/')
--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,38 @@
 window.onload = () => {
 	console.log("Life is pain!")
 }
+
+function folderclick(folder, currentFolder) {
+	var filepath = `${currentFolder}\\${folder}`
+	fetch("http://127.0.0.1:5000/set_cookie", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({"filepath": filepath, "currentFolder": currentFolder})
+	})
+}
+
+function folderback(lastfolder) {
+	if (lastfolder == "C:\\Users\\abbyl\\Downloads") {
+		fetch("http://127.0.0.1:5000/set_cookie", {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({"filepath": lastfolder, "currentFolder": lastfolder})
+		})
+		return;
+	}
+	parentFolder = lastfolder.split('\\')
+	parentFolder.pop()
+	lastfolder = lastfolder.replace('\\', '\\\\')
+	parentFolder = parentFolder.join('\\\\')
+	fetch("http://127.0.0.1:5000/set_cookie", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({"filepath": lastfolder, "currentFolder": parentFolder})
+	})
+}
